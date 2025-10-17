12e69edc3242080c441928c58a988f7752bee889
Pascal Ordano <pascalordanodev@gmail.com>
2025-10-16 22:41:49 -0300

    comments

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-
-
 const addTaskBtn = document.getElementById('addTaskButton');
 const taskInput = document.getElementById('taskInput');
 const taskList = document.getElementById('tasklist');
 
 addTaskBtn.addEventListener('click', addTask);
 
+// addEventListener(EVENTO A ESUCHAR, FUNCION A EJECUTAR CUANDO FINALMENTE LO ESUCHA);
 
 function addTask() {
-    const taskText = taskInput.value.trim();
-    if(taskText === "") {
-        alert("Please enter a task.");
+    const taskText = taskInput.value.trim(); // limpia el input. Si era ' hola ' => 'hola'
+    if(taskText === "") { // VALIDACION DEL INPUT => se fija que no este vacio
+        alert("Please enter a task.");  // TIRA ALERTA => no se puede hacer un todo vacio
         return;
     }
 
-    const li = document.createElement("li");
+    // si estamos aca -> entonces taskText != "" (vacio)
+    const li = document.createElement("li"); // CREAMOS UN LI 
 
-    const span = document.createElement("span");
-    span.textContent = taskText;
 
-    const deleteBtn = document.createElement("button");
+    const span = document.createElement("span");  // CREAMOS SPAN PARA QUE SEA EL TEXTO DEL LI
+    span.textContent = taskText;  // ASIGNAMOS, LO QUE ESCRIBIO EL USER (TASKTEXT) A EL SPAN 
+
+
+    const deleteBtn = document.createElement("button");  // CREA boton de cruz 
     deleteBtn.textContent = "X";
-    deleteBtn.classList.add("deleteBtn");
+    deleteBtn.classList.add("delete-btn");  // agrega la clase delete-btn al boton, esa clase esta en el styles.css y lo pone facha
 
-    deleteBtn.addEventListener("click", () => {
-        li.remove();
+    deleteBtn.addEventListener("click",  () => {
+        li.remove(); 
     });
 
     li.appendChild(span);
